abe76265ac1ff464c2c4e51de06d0549aafe117e
BlakeDrostRevature <blake.drost@revature.net>
2021-09-14 10:01:45 -0500

    SweetnSalty Coding Challenge Solution

---
+++ SweetnSaltyConsole/SweetnSaltyConsole/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SweetnSaltyConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int sweetCount = 0;
+          int saltyCount = 0;
+          int sweetnSaltyCount = 0;
+          for(int i = 1; i <= 1000; i++){
+            //Sweet and Salty Conditional
+            if(i % 3 == 0 && i % 5 == 0){
+              Console.Write("sweet’nSalty ");
+              sweetnSaltyCount++;
+            }
+            //Sweet Conditional
+            else if(i % 3 == 0){
+              Console.Write("sweet ");
+              sweetCount++;
+            }
+            //Salty Conditional
+            else if(i % 5 == 0){
+              Console.Write("salty ");
+              saltyCount++;
+            }
+            else{
+              Console.Write(i+" ");
+            }
+            if(i % 10 == 0){
+              Console.WriteLine();
+            }
+          }
+          Console.Write($"Sweet: {sweetCount}\nSalty: {saltyCount}\nSweet'nSalty: {sweetnSaltyCount}");
+        }
+    }
+}
